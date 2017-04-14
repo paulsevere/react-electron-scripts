8ed611738af0476bb7ed2dc0274fd816ba734397
Patrick OConnor <j.doe@example.com>
2017-04-14 12:14:11 -0400

    changes template

--- template/src/App.js
+++ template/src/App.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
 class App extends Component {
   render() {
