6199f69a0481b9a6cfaef060aa365411572c2b2d
Patrick OConnor <j.doe@example.com>
2017-04-14 13:04:26 -0400

    update v

--- template/src/App.js
+++ template/src/App.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
