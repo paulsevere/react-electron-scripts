9ceb8f8bfca75e6e79e487e724060106fbb7fea3
default <default@other-mbp.local>
2017-06-02 02:51:12 -0400

    fixed unhandled exception during electron install check

--- scripts/utils/electronCheck.js
+++ scripts/utils/electronCheck.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const { execSync } = require('child_process');
-const spawn = require('cross-spawn');
-const chalk = require('chalk');
-const prompt = require('react-dev-utils/prompt');
-const paths = require('../../config/paths');
-const fs = require('fs');
+const { execSync } = require("child_process");
+const spawn = require("cross-spawn");
+const chalk = require("chalk");
+const prompt = require("react-dev-utils/prompt");
+const paths = require("../../config/paths");
+const fs = require("fs");
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 
-const elec = commandExists('electron');
-const cli = useYarn ? 'yarn' : 'npm';
+const elec = () => commandExists("electron");
+const cli = useYarn ? "yarn" : "npm";
 
 function handlePromptForInstall(resolve, reject) {
   return function(ans) {
     if (ans) {
       try {
         if (useYarn) {
-          spawn.sync(cli, ['global', 'add', 'electron'], { stdio: 'inherit' });
+          spawn.sync(cli, ["global", "add", "electron"], { stdio: "inherit" });
         } else {
-          spawn.sync(cli, ['install', '-g', 'electron'], { stdio: 'inherit' });
+          spawn.sync(cli, ["install", "-g", "electron"], { stdio: "inherit" });
         }
       } catch (err) {
         reject(err);
@@ -27,14 +27,14 @@ function handlePromptForInstall(resolve, reject) {
       resolve();
     } else {
       console.log("Install on your own with 'npm i -g electron'");
-      reject(null);
+      resolve(null);
     }
   };
 }
 
 function commandExists(command) {
   try {
-    execSync('which ' + command);
+    execSync("which " + command);
   } catch (err) {
     return false;
   }
@@ -43,9 +43,9 @@ function commandExists(command) {
 
 function electronCheck() {
   return new Promise((resolve, reject) => {
-    if (!elec) {
-      console.log(chalk.red('Electron must be installed globally'));
-      prompt(chalk.green('Install now?'), true).then(
+    if (!elec()) {
+      console.log(chalk.red("Electron must be installed globally"));
+      prompt(chalk.green("Install now?"), true).then(
         handlePromptForInstall(resolve, reject)
       );
     } else {
